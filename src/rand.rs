998ebd6c9ca904c1e68929bbb1a9130bb4de5ccb
Ruihan Li <lrh2000@pku.edu.cn>
2024-09-19 16:19:09 +0800

    Replace all `pub(crate)` with `pub`
    This commit is generated from the following command:
    
    	find . -name "*.rs" | xargs sed -i 's|pub(crate)|pub|g'

--- src/rand.rs
+++ src/rand.rs
@@ -2,16 +2,16 @@
 #![allow(unused)]
 
 #[derive(Debug)]
-pub(crate) struct Rand {
+pub struct Rand {
     state: u64,
 }
 
 impl Rand {
-    pub(crate) const fn new(seed: u64) -> Self {
+    pub const fn new(seed: u64) -> Self {
         Self { state: seed }
     }
 
-    pub(crate) fn rand_u32(&mut self) -> u32 {
+    pub fn rand_u32(&mut self) -> u32 {
         // sPCG32 from https://www.pcg-random.org/paper.html
         // see also https://nullprogram.com/blog/2017/09/21/
         const M: u64 = 0xbb2efcec3c39611d;
@@ -24,12 +24,12 @@ impl Rand {
         (s >> shift) as u32
     }
 
-    pub(crate) fn rand_u16(&mut self) -> u16 {
+    pub fn rand_u16(&mut self) -> u16 {
         let n = self.rand_u32();
         (n ^ (n >> 16)) as u16
     }
 
-    pub(crate) fn rand_source_port(&mut self) -> u16 {
+    pub fn rand_source_port(&mut self) -> u16 {
         loop {
             let res = self.rand_u16();
             if res > 1024 {
